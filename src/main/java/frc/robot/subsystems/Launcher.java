9110785bae9180276a1c38b74bb1f1e45f626080
Colin Carlson <nhlaxb@gmail.com>
2026-01-13 21:50:29 -0500

    Merged main in, and cleaned up typos

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -3,11 +3,11 @@ package frc.robot.subsystems;
 import frc.robot.Constants;
 
 public class Launcher {
-    private ChaosTalonFx m_launcherMotor=new ChaosTalonFx(Constants.LauncherCanId, Constants.LauncherCanBus) ;
+    private ChaosTalonFx m_launcherMotor = new ChaosTalonFx(Constants.LauncherCanId, Constants.LauncherCanBus);
 
     public Launcher () {} 
 
     public void LauncherMotor100percent () {
-        m_launerMotor.setspeed (1.0);
+        m_launcherMotor.setSpeed(1.0);
     }
 }
