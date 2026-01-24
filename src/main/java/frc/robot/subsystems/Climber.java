9dcd47d831c5d3c638681f44051776e08039d722
CHAOS 131 <131.chaos@gmail.com>
2026-01-24 14:47:35 -0500

    Added climber config and safety

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,22 +4,50 @@
 
 package frc.robot.subsystems;
 
-import com.chaos131.util.ChaosTalonFx;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Meters;
 
+import com.chaos131.util.ChaosTalonFx;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
+  private ChaosTalonFx m_climberMotor = new ChaosTalonFx(ClimberConstants.ClimberCanId, ClimberConstants.ClimberCanBus);
+
   /** Creates a new Climber. */
-  public Climber() {}
+  public Climber() {
+    m_climberMotor.Configuration.Feedback.RotorToSensorRatio = ClimberConstants.RotorToSensorRatio;
+    m_climberMotor.Configuration.Feedback.SensorToMechanismRatio = ClimberConstants.SensorToMechanismRatio;
+    m_climberMotor.Configuration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor; // TODO: double check
 
- private ChaosTalonFx m_climberMotor = new ChaosTalonFx(ClimberConstants.ClimberCanId, ClimberConstants.ClimberCanBus);
-
- public void setClimberSpeed (double speed) {
-        m_climberMotor.setSpeed(speed);
-    }
+    m_climberMotor.Configuration.CurrentLimits.SupplyCurrentLimit = ClimberConstants.SupplyCurrentLimit.in(Amps);
+    m_climberMotor.Configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
+    m_climberMotor.Configuration.CurrentLimits.StatorCurrentLimit = ClimberConstants.StatorCurrentLimit.in(Amps);
+    m_climberMotor.Configuration.CurrentLimits.StatorCurrentLimitEnable = true;
     
-    public double getClimberSpeed () {
-        return m_climberMotor.get();
+    m_climberMotor.Configuration.MotorOutput.Inverted = ClimberConstants.MotorDirection;
+    m_climberMotor.Configuration.MotorOutput.NeutralMode = ClimberConstants.NeutralMode;
+
+    m_climberMotor.applyConfig();
+  }
+
+  public Distance getHeight() {
+    return Meters.of(m_climberMotor.getPosition().getValueAsDouble());
+  }
+
+  public void setClimberSpeed(double speed) {
+    if (getHeight().lt(ClimberConstants.MinExtension) && speed < 0) {
+      speed = 0;
+    } else if (getHeight().gt(ClimberConstants.MaxExtension) && speed > 0) {
+      speed = 0;
     }
+    m_climberMotor.setSpeed(speed);
+  }
+
+  public double getClimberSpeed() {
+    return m_climberMotor.get();
+  }
 }
