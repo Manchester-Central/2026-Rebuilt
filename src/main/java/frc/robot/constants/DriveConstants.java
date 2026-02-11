9ea7c8e9dc9f9b915330ac82bef5463e3ef75c8e
Joshua Allard <jnallard@users.noreply.github.com>
2026-02-10 21:58:55 -0500

    Adds DriveCommandConstants

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -8,8 +8,11 @@ import static edu.wpi.first.units.Units.KilogramSquareMeters;
 import static edu.wpi.first.units.Units.Kilograms;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
 
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.path.PathConstraints;
@@ -20,12 +23,30 @@ import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.measure.Mass;
 import edu.wpi.first.units.measure.MomentOfInertia;
+import edu.wpi.first.units.measure.Time;
+import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
 
 /** Constants for the drive.
  * @see TunerConstants TunerConstants for variables controlled by the CTRE generator, like CAN IDs, module PID values, etc.
  */
 public final class DriveConstants {
+
+  /** Commands for the DriveCommands class
+   * @see DriveCommands DriveCommands
+   */
+  public static final class DriveCommandConstants {
+    public static final double Deadband = 0.1;
+    public static final double RotationKp = 5.0;
+    public static final double RotationKi = 0.0;
+    public static final double RotationKd = 0.4;
+    public static final AngularVelocity RotationMaxVelocity = RadiansPerSecond.of(8.0);
+    public static final AngularAcceleration RotationMaxAcceleration = RadiansPerSecondPerSecond.of(20.0);
+    public static final Time FeedForwardStartDelay = Seconds.of(2.0); // Secs
+    public static final double FeedForwardRampRate = 0.1; // Volts/Sec
+    public static final AngularVelocity WheelRadiusMaxVelocity = RadiansPerSecond.of(0.25);
+    public static final double WheelRadiusRampRate = 0.05; // Rad/Sec^2
+  }
 
   // PathPlanner config constants - these values should match the settings in the PathPlanner UI. 
   public static final Mass RobotMass = Kilograms.of(74.088); // TODO: confirm
