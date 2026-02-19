37e7ff27e2b22f331de5aa7f6648f280d9f444b7
CHAOS <131.chaos@gmail.com>
2026-02-19 18:52:41 -0500

    Offsets for quest

--- src/main/java/frc/robot/constants/QuestConstants.java
+++ src/main/java/frc/robot/constants/QuestConstants.java
@@ -4,14 +4,15 @@
 
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.units.measure.Distance;
 
 public final class QuestConstants {
-  public static final Distance RobotToQuestXInches = Inches.of(5); // TODO: Get Value from CAD
-  public static final Distance RobotToQuestYInches = Inches.of(5); // TODO: Get Value from CAD
-  public static final Distance RobotToQuestZInches = Inches.of(5); // TODO: Get Value from CAD
-  public static final Rotation3d RobotToQuestRotation = Rotation3d.kZero; // TODO: Get Value from CAD
+  public static final Distance RobotToQuestXInches = Inches.of(8.25); // TODO: Get Value from CAD
+  public static final Distance RobotToQuestYInches = Inches.of(-12.75); // TODO: Get Value from CAD
+  public static final Distance RobotToQuestZInches = Inches.of(12); // TODO: Get Value from CAD
+  public static final Rotation3d RobotToQuestRotation = new Rotation3d(Degrees.of(0),Degrees.of(0),Degrees.of(-90)); // TODO: Get Value from CAD
 }
