7b467857107c9cc678f8df2ce9d54241cb6e5c4a
jnallard <joshuaal2000@gmail.com>
2026-03-08 22:31:56 -0400

    Cleans up Flywheel Table class

--- src/main/java/frc/robot/subsystems/launcher/FlywheelTable.java
+++ src/main/java/frc/robot/subsystems/launcher/FlywheelTable.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems.launcher;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
+import java.util.List;
+
 import com.chaos131.tables.LookupTable;
 
 import edu.wpi.first.units.DistanceUnit;
@@ -14,6 +16,23 @@ import edu.wpi.first.units.measure.Distance;
 
 /** Add your docs here. */
 public class FlywheelTable extends LookupTable<DistanceUnit, Distance, TableRow> {
+
+  private static final List<TableRow> Rows = List.of(
+    new TableRow(Meters.of(1), MetersPerSecond.of(24)),
+    new TableRow(Meters.of(2), MetersPerSecond.of(32)),
+    new TableRow(Meters.of(3),  MetersPerSecond.of(38))
+  );
+
+  private static final FlywheelTable m_instance = new FlywheelTable(Rows);
+
+  public static FlywheelTable getInstance() {
+    return m_instance;
+  }
+
+  private FlywheelTable(List<TableRow> rows) {
+    super(rows);
+  }
+
   @Override
   public TableRow mergeRows(Distance targetMeasure, TableRow row1, TableRow row2) {
     var linearVelocity =
@@ -23,14 +42,5 @@ public class FlywheelTable extends LookupTable<DistanceUnit, Distance, TableRow>
             row2,
             (row) -> ((TableRow) row).getLaunchSpeed().in(MetersPerSecond));
     return new TableRow(targetMeasure, MetersPerSecond.of(linearVelocity));
-  }
-
-  private static LookupTable<DistanceUnit, Distance, TableRow> m_instance = new FlywheelTable()
-    .addRow(new TableRow(Meters.of(1), MetersPerSecond.of(24)))
-    .addRow(new TableRow(Meters.of(1), MetersPerSecond.of(32)))
-    .addRow(new TableRow(Meters.of(3),  MetersPerSecond.of(38)));
-
-  public static LookupTable<DistanceUnit, Distance, TableRow> getInstance() {
-    return m_instance;
   }
 }
