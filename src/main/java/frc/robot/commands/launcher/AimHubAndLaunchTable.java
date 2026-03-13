5308af5b79882518296381db93ec0ebaea62954b
CHAOS 131 <131.chaos@gmail.com>
2026-03-13 16:45:50 -0400

    intake pivot while launch

--- src/main/java/frc/robot/commands/launcher/AimHubAndLaunchTable.java
+++ src/main/java/frc/robot/commands/launcher/AimHubAndLaunchTable.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.launcher;
 
+import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
@@ -12,7 +13,9 @@ import java.util.Optional;
 import com.chaos131.poses.FieldPose;
 import com.chaos131.poses.FieldPose2026;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.FieldDimensions;
 import frc.robot.constants.LauncherConstants.FeederConstants;
 import frc.robot.subsystems.interfaces.IDrive;
@@ -26,9 +29,18 @@ import frc.robot.subsystems.launcher.TableRow;
 public class AimHubAndLaunchTable extends BaseLaunchCommand {
   private TableRow m_flywheelTableRow = new TableRow(Inches.of(0), MetersPerSecond.of(0), 0.0);
 
+  private Timer m_intakeTimer = new Timer();
+
   public AimHubAndLaunchTable(ILauncher launcher, IDrive swerveDrive, IIntake intake) {
     super(launcher, swerveDrive, intake);
   }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    m_intakeTimer.restart();
+  }
+
 
   @Override
   protected Optional<FieldPose> getTargetPose() {
@@ -54,4 +66,11 @@ public class AimHubAndLaunchTable extends BaseLaunchCommand {
   protected void enableFeederForLauncher() {
     m_launcher.setFeederSpeed(FeederConstants.BottomFeederSpeed.get(), FeederConstants.TopFeederSpeed.get()); 
   }
+
+  @Override
+  public Angle getIntakePivotAngle(){
+    Angle pivotAngle = super.getIntakePivotAngle();
+    return pivotAngle.minus(Degree.of(m_intakeTimer.get()));
+  }
+
 }
