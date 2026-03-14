a97681de89db81511957d368a04b9276fd804444
CHAOS 131 <131.chaos@gmail.com>
2026-03-14 11:17:19 -0400

    intake swaps angle no timer

--- src/main/java/frc/robot/constants/LauncherConstants.java
+++ src/main/java/frc/robot/constants/LauncherConstants.java
@@ -67,7 +67,7 @@ public final class LauncherConstants {
 
   public static final DashboardNumber LauncherSpeed = new DashboardNumber("Launcher/ManualLaunchSpeed", 0.6); 
   public static final DashboardNumber UnjamSpeed = new DashboardNumber("Launcher/UnjamSpeed", -0.3); 
-  public static final DashboardNumber IntakePivotTimerSpeed = new DashboardNumber("Launcher/Intake/IntakePivotTimer", 30);
+  public static final DashboardUnit <AngleUnit, Angle> IntakePivotJostleAngle = new DashboardUnit<>("Launcher/Intake/IntakePivotJostleAngle", Degrees.of(125));
 
   public static final Distance LauncherToHubHeight = FieldDimensions.HubHeight.minus(LauncherHeight);
 
