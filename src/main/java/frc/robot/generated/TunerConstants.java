b2ecc7f5c3f4683abb90e33dfceb2c60000edfaf
CHAOS <131.chaos@gmail.com>
2026-05-30 18:09:34 -0400

    battle cry day one

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -177,7 +177,7 @@ public class TunerConstants {
     private static final int kFrontRightDriveMotorId = 4;
     private static final int kFrontRightSteerMotorId = 5;
     private static final int kFrontRightEncoderId = 6;
-    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.146728515625);
+    private static final Angle kFrontRightEncoderOffset = Rotations.of(0.3601);
     private static final boolean kFrontRightSteerMotorInverted = true;
     private static final boolean kFrontRightEncoderInverted = false;
 
